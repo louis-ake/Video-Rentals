3c7009979f2a030c7d2bd793c56996c5a616085c
louis-ake <louis.akerman@outlook.com>
2020-06-23 19:10:20 +0100

    tests added for returns route

--- routes/returns.js
+++ routes/returns.js
@@ -1,3 +1,4 @@
+const { Rental } = require('../models/rentals');
 const express = require('express');
 const router = express.Router();
 const asyncMiddleware = require('../middleware/async');
@@ -5,6 +6,15 @@ const asyncMiddleware = require('../middleware/async');
 router.post('/', asyncMiddleware(async(req, res) => {
     if (!req.body.customerId) return res.status(400).send('customerId not provided');
     if (!req.body.movieId) return res.status(400).send('movieId not provided');
+
+    const rental = await Rental.findOne({
+        'customer._id': req.body.customerId,
+        'movie._id': req.body.movieId
+    });
+
+    if (!rental) return res.status(404).send('Rental not found');
+    
+    if (rental.dateReturned !== undefined) return res.status(400).send('Rental already processed');
 
     res.status(401).send('unauthorised');
 }));
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -68,5 +68,20 @@ describe('/api/returns', () => {
         expect(res.status).toBe(400);
     });
 
-    
+    it('should return 404 if no rental found for this customer/movie', async () => {
+        await Rental.remove({});
+
+        const res = await exec();
+
+        expect(res.status).toBe(404);
+    });
+
+    it('should return 400 if rental already processed', async () => {
+        rental.dateReturned = new Date();
+        await rental.save();
+
+        const res = await exec();
+
+        expect(res.status).toBe(400);
+    });
 });
